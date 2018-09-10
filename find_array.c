3ff653e23aea57b8f75ab7032f91cd67c8407db4
15ec014 <gowthamsmart98@gmail.com>
2018-09-10 14:43:40 +0530

    Create find_array.c

---
+++ find_array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+int n,t,i,j,a[20];
+printf("enter the array of numbers");
+scanf("%d",&n);
+printf("enter the numbers");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}
+}
+for(i=0;i<n;i++)
+{
+    printf("%d",a[i]);
+}
+}
